960db401e10b34bf08ca6eb5dba331be354ee45f
Kim <ugkimm@naver.com>
2021-12-15 22:05:29 +0200

    add updated

---
+++ fd.py
@@ -0,0 +1,351 @@
+import os
+import numpy as np
+from scipy import linalg
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torchvision.models.inception import inception_v3
+
+
+# +
+class InceptionV3(nn.Module):
+    from torchvision.models.inception import inception_v3
+    """Pretrained InceptionV3 network returning feature maps"""
+
+    # Index of default block of inception to return,
+    # corresponds to output of final average pooling
+    DEFAULT_BLOCK_INDEX = 3
+
+    # Maps feature dimensionality to their output blocks indices
+    BLOCK_INDEX_BY_DIM = {
+        64: 0,   # First max pooling features
+        192: 1,  # Second max pooling featurs
+        768: 2,  # Pre-aux classifier features
+        2048: 3  # Final average pooling features
+    }
+
+    def __init__(self,
+                 output_blocks=[DEFAULT_BLOCK_INDEX],
+                 resize_input=True,
+                 normalize_input=True,
+                 requires_grad=False):
+        
+        super(InceptionV3, self).__init__()
+
+        self.resize_input = resize_input
+        self.normalize_input = normalize_input
+        self.output_blocks = sorted(output_blocks)
+        self.last_needed_block = max(output_blocks)
+
+        assert self.last_needed_block <= 3, \
+            'Last possible output block index is 3'
+
+        self.blocks = nn.ModuleList()
+
+        
+        inception = inception_v3(pretrained=True)
+
+        # Block 0: input to maxpool1
+        block0 = [
+            inception.Conv2d_1a_3x3,
+            inception.Conv2d_2a_3x3,
+            inception.Conv2d_2b_3x3,
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        ]
+        self.blocks.append(nn.Sequential(*block0))
+
+        # Block 1: maxpool1 to maxpool2
+        if self.last_needed_block >= 1:
+            block1 = [
+                inception.Conv2d_3b_1x1,
+                inception.Conv2d_4a_3x3,
+                nn.MaxPool2d(kernel_size=3, stride=2)
+            ]
+            self.blocks.append(nn.Sequential(*block1))
+
+        # Block 2: maxpool2 to aux classifier
+        if self.last_needed_block >= 2:
+            block2 = [
+                inception.Mixed_5b,
+                inception.Mixed_5c,
+                inception.Mixed_5d,
+                inception.Mixed_6a,
+                inception.Mixed_6b,
+                inception.Mixed_6c,
+                inception.Mixed_6d,
+                inception.Mixed_6e,
+            ]
+            self.blocks.append(nn.Sequential(*block2))
+
+        # Block 3: aux classifier to final avgpool
+        if self.last_needed_block >= 3:
+            block3 = [
+                inception.Mixed_7a,
+                inception.Mixed_7b,
+                inception.Mixed_7c,
+                nn.AdaptiveAvgPool2d(output_size=(1, 1))
+            ]
+            self.blocks.append(nn.Sequential(*block3))
+
+        for param in self.parameters():
+            param.requires_grad = requires_grad
+
+    def forward(self, inputs):
+        """Get Inception feature maps
+        Parameters
+        ----------
+        inp : torch.autograd.Variable
+            Input tensor of shape Bx3xHxW. Values are expected to be in
+            range (0, 1)
+        Returns
+        -------
+        List of torch.autograd.Variable, corresponding to the selected output
+        block, sorted ascending by index
+        """
+        outputs = []
+        x = inputs
+
+        if self.resize_input:
+            x = F.interpolate(x,
+                              size=(299, 299),
+                              mode='bilinear',
+                              align_corners=False)
+
+        if self.normalize_input:
+            x = 2 * x - 1  # Scale from range (0, 1) to range (-1, 1)
+
+        for idx, block in enumerate(self.blocks):
+            x = block(x)
+            if idx in self.output_blocks:
+                outputs.append(x)
+
+            if idx == self.last_needed_block:
+                break
+
+        return outputs
+    
+block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
+inception_model = InceptionV3([block_idx])
+
+# -
+
+def calculate_activation_statistics(images, model, dims=2048, cuda=False):
+    from torch.nn.functional import adaptive_avg_pool2d
+    model.eval()
+    
+    act=np.empty((len(images), dims))
+    
+    if cuda:
+        batch=images.cuda()
+    else:
+        batch=images
+    
+    pred = model(batch)[0]
+
+    # If model output is not scalar, apply global spatial average pooling.
+    # This happens if you choose a dimensionality not equal 2048.
+    if pred.size(2) != 1 or pred.size(3) != 1:
+        pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+    act= pred.cpu().data.numpy().reshape(pred.size(0), -1)
+    
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians 
+    X_1 ~ N(mu_1, C_1) and X_2 ~ N(mu_2, C_2) is 
+        d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+    
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
+
+
+def calculate_fid(real_images, fake_images, model=inception_model):
+    
+    mu1, sigma1 = calculate_activation_statistics(real_images, model, cuda=False)
+    mu2, sigma2 = calculate_activation_statistics(fake_images, model, cuda=False)
+    
+    """get fretched distance"""
+    fid_value = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+    return fid_value
+
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)
+        self.fc1 = nn.Linear(32*4*4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    @classmethod
+    def pretrained(cls):
+        model = cls()
+        model.load_state_dict(torch.load('classifier.pth', map_location=lambda storage, loc: storage))
+        return model
+
+    def forward(self, x):
+        """
+        Args:
+          x of shape (batch_size, 1, 28, 28): Input images.
+        
+        Returns:
+          y of shape (batch_size, 10): Outputs of the network.
+        """
+        x = self.conv1(x)
+        x = self.pool(F.relu(x))
+        x = self.conv2(x)
+        x = self.pool(F.relu(x))
+        x = x.view(-1, self.fc1.in_features)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def get_activations(self, x):
+        """
+        Args:
+          x of shape (batch_size, 1, 28, 28): Input images.
+        
+        Returns:
+          y of shape (batch_size, 10): Outputs of the network.
+        """
+        x = self.conv1(x)
+        x = self.pool(F.relu(x))
+        x = self.conv2(x)
+        x = self.pool(F.relu(x))
+        x = x.view(-1, self.fc1.in_features)
+        x = F.relu(self.fc1(x))
+        #x = F.relu(self.fc2(x))
+        #x = self.fc3(x)
+        return x
+
+
+def frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    Stable version by Dougal J. Sutherland.
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+    Returns:
+    --   : The Frechet Distance.
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, 'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, 'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
+
+
+class FDScore(nn.Module):
+    def __init__(self):
+        super(FDScore, self).__init__()
+        self.net = net = Classifier.pretrained()
+        self.net.eval()
+        
+    def train(self, trainset, batch_size):
+        'Compute statistic from training set'
+        dataloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size)
+        device = next(self.net.parameters()).device
+        
+        n = 0
+        self.mu = 0
+        self.sigma = 0
+        for batch in dataloader:
+            samples = batch[0].to(device)
+            activations = self.get_activations(samples)
+
+            batch_size = samples.size(0)
+            w_old = n / (n + batch_size)
+            w_new = batch_size / (n + batch_size)
+
+            self.mu = w_old * self.mu + w_new * np.mean(activations, axis=0)
+            self.sigma = w_old * self.sigma + w_new * np.cov(activations, rowvar=False)
+            n = n + batch_size
+
+    def get_activations(self, samples):
+        with torch.no_grad():
+            return self.net.get_activations(samples).cpu().numpy()
+
+    def calculate(self, samples):
+        minmax = np.array([samples.cpu().numpy().min(), samples.cpu().numpy().max()])
+        if not np.allclose(minmax, np.array([-1., 1.]), rtol=0.1, atol=0.1):
+            print('Warning: the pixel values should be in the range [-1, 1]. The range in the samples is %s.' % minmax)
+        act_fake = self.get_activations(samples)
+        mu_fake = np.mean(act_fake, axis=0)
+        sigma_fake = np.cov(act_fake, rowvar=False)
+
+        return frechet_distance(mu_fake, sigma_fake, self.mu, self.sigma)
